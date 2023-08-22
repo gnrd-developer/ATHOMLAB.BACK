9d295c5a4788c63ee611cf5d7a545d3452f4b508
gonrodriguezd <gon.rodriguezd@duocuc.cl>
2023-08-22 17:38:48 -0400

    tercer intento

--- src/main/java/com/souldev/cart/security/util/CookieUtil.java
+++ src/main/java/com/souldev/cart/security/util/CookieUtil.java
@@ -12,7 +12,7 @@ public class CookieUtil {
             Integer maxAge, String domain) {
         Cookie cookie = new Cookie(name, value);
         cookie.setSecure(secure);
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
         cookie.setMaxAge(maxAge);
         cookie.setDomain(domain);
         cookie.setPath("/");
@@ -22,7 +22,7 @@ public class CookieUtil {
     public static void clear(HttpServletResponse httpServletResponse, String name) {
         Cookie cookie = new Cookie(name, null);
         cookie.setPath("/");
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
         cookie.setMaxAge(1);
         cookie.setDomain("localhost");
         httpServletResponse.addCookie(cookie);

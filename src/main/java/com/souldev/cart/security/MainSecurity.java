d448832c621e16c96c27993ee5c29c6f611e9b37
gnrd-developer <gnrd.developer@gmail.com>
2023-10-19 20:33:04 -0300

    19-10-23-v5

--- src/main/java/com/souldev/cart/security/MainSecurity.java
+++ src/main/java/com/souldev/cart/security/MainSecurity.java
@@ -70,7 +70,7 @@ public class MainSecurity {
             return configuration;
         }).and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login","/auth/register","/auth/logout","/auth/detail/**","/product/**","/category/**","/subcategory/**","/product/subcategory/**")
+                .antMatchers("/auth/login","/auth/register","/auth/logout","/auth/details/**","/product/**","/category/**","/subcategory/**","/product/subcategory/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
